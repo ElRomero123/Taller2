8859fc25d4732d0d7d60428ad48347728acee800
Juan Pablo Romero Londoño <juan.romerol@upb.edu.co>
2018-02-19 18:19:42 -0500

    Creación del proyecto.

---
+++ ControlEventos/ControlEventos/FormularioRegistro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlEventos
+{
+    public partial class FormularioRegistro : Form
+    {
+        public FormularioRegistro()
+        {
+            InitializeComponent();
+        }
+    }
+}
